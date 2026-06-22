2ee69a39914056335374b50e515bcdf0927ab931
Kumkum <kumkumdadhich7@gmail.com>
2026-06-22 13:36:06 +0530

    feat: add routing for dashboard and HRMS modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
+
+import DashboardPage from "./pages/DashboardPage";
+import EmployeePage from "./pages/EmployeePage";
+import AttendancePage from "./pages/AttendancePage";
 import PayrollPage from "./pages/PayrollPage";
 import LeavePage from "./pages/LeavePage";
 
-
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -13,9 +17,17 @@ function App() {
       <div style={{ flex: 1 }}>
         <Navbar />
 
-        <PayrollPage />
+        <Routes>
+          <Route path="/" element={<DashboardPage />} />
 
-        <LeavePage />
+          <Route path="/employees" element={<EmployeePage />} />
+
+          <Route path="/attendance" element={<AttendancePage />} />
+
+          <Route path="/payroll" element={<PayrollPage />} />
+
+          <Route path="/leave" element={<LeavePage />} />
+        </Routes>
       </div>
     </div>
   );
